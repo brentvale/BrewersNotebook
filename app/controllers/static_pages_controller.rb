33ee2a0f258fe839e1075c502496bebb3e4bb7fc
Brent Vale <brentvale@gmail.com>
2017-08-04 09:50:06 -0700

    stylizes devise forms

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,4 +3,10 @@ class StaticPagesController < ApplicationController
   def home
   end
   
+  def terms_of_service
+  end
+  
+  def payment_terms_of_service
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
     resources :batches, only: [:index]
   end
   
+  get '/terms_of_service', to: 'static_pages#terms_of_service'
+  get '/payment_terms_of_service', to: 'static_pages#payment_terms_of_service'
+  
   root to: "static_pages#home"
 end
